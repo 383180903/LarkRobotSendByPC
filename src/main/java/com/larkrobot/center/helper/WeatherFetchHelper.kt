1a0c948bf937519e0de581d0c701e7a2573f2ff4
wuxiaofeng <wuxiaofeng@lizhi.fm>
2021-06-19 18:18:56 +0800

    feat:新增天气建议

--- src/main/java/com/larkrobot/center/helper/WeatherFetchHelper.kt
+++ src/main/java/com/larkrobot/center/helper/WeatherFetchHelper.kt
@@ -4,6 +4,7 @@ import net.sf.json.JSONObject
 import okhttp3.*
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
+import java.io.StringReader
 
 /**
  * 天气获取的类
@@ -41,18 +42,26 @@ object WeatherFetchHelper {
             val city = opt.opt("city")
             val date = opt.opt("date")
             val week = opt.opt("week")
-            val weather = opt.opt("weather")
+            val weather = opt.optString("weather")
             val temp = opt.opt("temp")
             val tempHigh = opt.opt("temphigh")
             val tempLow = opt.opt("templow")
             val windSpeed = opt.opt("windspeed")
+
+            val advice = if(weather.contains("雨")){
+                "建议带伞出门哦"
+            }else {
+                "早走早享受"
+            }
             val sendText = "今日天气预报：\n" +
                     "城市：$city\n" +
                     "日期：$date $week\n" +
                     "天气：$weather\n" +
                     "当前温度：$temp℃\n" +
                     "温度区间：$tempLow ~ $tempHigh℃\n" +
-                    "风速：$windSpeed"
+                    "风速：$windSpeed\n" +
+                    "出行建议：$advice"
+
             println(sendText)
             LarkRobotSendHelper.sendTextMessage(TextMessageBuilder.buildNormalText(sendText))
         }
